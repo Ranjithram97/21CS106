ac47bd558aee42cb9dbd9926dd0101b193b384e6
Sriram Babu T <127068774+Ranjithram97@users.noreply.github.com>
2024-05-02 13:17:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AverageCalculator from './AverageCalculator'; 
+
+function App() {
+  return (
+    <div className="App">
+        <AverageCalculator /> {/* Render the AverageCalculator component */}
+    </div>
+  );
+}
+
+export default App;
